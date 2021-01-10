6aea10b8ce14cfc0fd4bf400e50172337c4f1862
Lis258 <avb-90@ya.ru>
2021-01-10 16:53:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double index = service.calculate(60, 160);
+        System.out.println(index);
+
+    }
+
+}
